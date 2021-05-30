051c9717a3c04b7102393155cd71eb7157562adf
Samir Prakash <Samir.Prakash@gmail.com>
2021-05-30 13:36:12 -0500

    setting up k8s config for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import express from 'express';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi There!');
+});
+
 app.listen(3000, () => {
   console.log('listening on port: 3000');
 });
