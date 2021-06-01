1ae9ae6cbee7dadba7ee72463037fdc2a6082f26
Samir Prakash <Samir.Prakash@gmail.com>
2021-06-01 15:02:56 -0500

    hash password before save

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // UserAttrs describes the properties that are required to create a user
 interface UserAttrs {
@@ -26,6 +27,13 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
