88a9af0c1cf76b01af9bac3b87524ab11ed28275
Samir Prakash <Samir.Prakash@gmail.com>
2021-05-31 20:51:32 -0500

    abstract custom error class

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,9 +1,18 @@
 import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-export class RequestValidatorError extends Error {
+export class RequestValidatorError extends CustomError {
+  statusCode = 400;
+
   constructor(public errors: ValidationError[]) {
-    super();
+    super('invalid email or password');
 
     Object.setPrototypeOf(this, RequestValidatorError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
   }
 }
--- auth/src/middleware/error-handler.ts
+++ auth/src/middleware/error-handler.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
-import { RequestValidatorError } from '../errors/request-validation-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
   err: Error,
@@ -8,15 +7,8 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidatorError) {
-    const formattedErrors = err.errors.map((error) => {
-      return { message: error.msg, field: error.param };
-    });
-    return res.status(400).send({ errors: formattedErrors });
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({ errors: [{ message: err.reason }] });
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({ errors: [{ message: 'something went wrong' }] });
