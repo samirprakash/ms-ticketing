f683697def34d30c499390a9b5c37095e532beeb
Samir Prakash <Samir.Prakash@gmail.com>
2021-06-06 17:11:07 -0500

    create a new user

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Sign Up</h1>
+      <div className='form-group'>
+        <label htmlFor='email'>Email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type='email'
+          name='email'
+          placeholder='Enter your email address'
+          className='form-control'
+        />
+      </div>
+      <div className='form-group'>
+        <label htmlFor='password'>Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type='password'
+          name='password'
+          placeholder='Enter your password'
+          className='form-control'
+        />
+      </div>
+      {errors.length > 0 && (
+        <div className='alert alert-danger'>
+          <h4>Ooops....</h4>
+          <ul className='my-0'>
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <button className='btn btn-primary'>Sign Up</button>
+    </form>
+  );
+};
+
+export default SignUp;
