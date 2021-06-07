19bee89f6dd0a6b96019ce8a4aaee15def656204
Samir Prakash <Samir.Prakash@gmail.com>
2021-06-07 18:41:15 -0500

    fixed path for auth service to use the npm package instead of file reference

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,9 @@
+import { errorHandler, NotFoundError } from '@sprockets/common';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
 import 'express-async-errors';
 import mongoose from 'mongoose';
-import { NotFoundError } from './errors/not-found-error';
-import { errorHandler } from './middlewares/error-handler';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
