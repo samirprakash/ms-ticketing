77bdc2efdd1fc87b1e951d026b97e347c808b780
Samir Prakash <Samir.Prakash@gmail.com>
2021-06-15 20:38:26 -0500

    updated order service CRUD

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,19 @@
-import { requireAuth, validateRequest } from '@sprockets/common';
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@sprockets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { Order } from '../models/order';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
+
+const ORDER_EXPIRATION_SECONDS = 15 * 60;
 
 router.post(
   '/api/orders',
@@ -17,7 +27,54 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database.
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // const isReserved = await ticket.isReserved();
+    // if (isReserved) {
+    //   throw new BadRequestError('Ticket is already reserved');
+    // }
+
+    // Ensure that the ticket is not already reserved
+    // Run a query for all orders and try to find a order that has an associated ticket
+    // which is same as the ticket we are looking for *and* check that the order is not cancelled.
+    // If we find a order, that means that the ticket has been reserved
+    const existingOrder = await Order.findOne({
+      ticket: this,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.Complete,
+          OrderStatus.AwaitingPayment,
+        ],
+      },
+    });
+    if (existingOrder) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // Set an expiration time for the order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + ORDER_EXPIRATION_SECONDS);
+
+    // build and save the order
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    await order.save();
+
+    // publish an event that the order has been created
+
+    res.status(201).send(order);
   }
 );
 
